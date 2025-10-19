3aa5cad27ce9ac3c2d14ed8314c87793a1d011dc
Viku4780 <vikrantmom5748@gmail.com>
2025-10-19 18:14:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,10 +8,7 @@ export async function connectDb() {
       throw new Error("❌ MONGO_URI is undefined. Check Railway Variables.");
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URI);
 
     console.log("✅ MongoDB connected successfully");
   } catch (err) {
