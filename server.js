e041af905194b1a51e81c4e34e29bc5dfd01fd33
Viku4780 <vikrantmom5748@gmail.com>
2025-10-19 16:41:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://your-frontend-url.up.railway.app",
     credentials: true
 }));
 
